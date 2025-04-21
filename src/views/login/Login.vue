0507094a67998db92d826b1f88f8cf25b25a5a8e
ys1112 <17719994218@163.com>
2025-04-21 16:49:48 +0800

    登录页面布局修改

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -287,7 +287,7 @@ const handleClick = (tab: TabsPaneContext, event: Event) => {
       justify-content: space-between;
       align-items: center;
       margin: 0 auto;
-      padding: 0 64px;
+      padding: 0 40px;
 
       span {
         font-size: 14px;
